53c517c4a29d36c66702b48ebe130bacc0d5ec09
Walter Meyer <wgmeyer@gmail.com>
2017-10-30 17:38:17 +0000

    Forgot to add new toplevel

--- gantry/gantry.ml
+++ gantry/gantry.ml
@@ -16,10 +16,10 @@ let _ =
           (* Semant.check ast; *)
           match action with
             | Ast -> print_string (Ast.string_of_program ast)
-            (* | LLVM_IR ->
+            | LLVM_IR ->
 		print_string (Llvm.string_of_llmodule (Codegen.translate ast))
             | Compile ->
 		let m = Codegen.translate ast in
                         Llvm_analysis.assert_valid_module m;
                         print_string (Llvm.string_of_llmodule m);;
-	     *)
+
