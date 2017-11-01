7b7bebae0485b2f29ad6bd13d09d44541daa34dd
audsc <audreyscopeland@gmail.com>
2017-11-01 00:42:11 +0000

    add print float and fix print_s and print_i

--- gantry/codegen.ml
+++ gantry/codegen.ml
@@ -62,7 +62,8 @@ in
   let builder = L.builder_at_end context (L.entry_block the_function) in
 
   let int_format_str = L.build_global_stringptr "%d\n" "fmt" builder
-  and str_format_str = L.build_global_stringptr "%s\n" "fmt" builder in
+  and str_format_str = L.build_global_stringptr "%s\n" "fmt" builder
+  and float_format_str = L.build_global_stringptr "%f\n" "fmt" builder in
     (* Construct the function's "locals": formal arguments and locally
        declared variables.  Allocate each on the stack, initialize their
        value, if appropriate, and remember their values in the "locals" map *)
@@ -118,12 +119,15 @@ in
 	       A.Neg 	-> L.build_neg
 	     | A.Not 	-> L.build_not) e' "tmp" builder
 	(*| A.Assign(s, e) -> let e' = expr builder e in ignore (L.build_store e' (lookup s) builder); e'*)
-	| A.FunExp("print", [e]) ->
+	| A.FunExp("print_i", [e]) ->
 	    L.build_call printf_func [| int_format_str ; (expr builder e) |]
-	    "printf" builder
-	| A.FunExp("prints", [e]) ->
+	    "print_i" builder
+	| A.FunExp("print_s", [e]) ->
 	    L.build_call printf_func [| str_format_str ; (expr builder e) |]
-	    "prints" builder
+	    "print_s" builder
+	| A.FunExp("print_d", [e]) ->
+	    L.build_call printf_func [| float_format_str ; (expr builder e) |]
+	    "print_d" builder
 	| A.FunExp(f, act) ->
 	    let (fdef, fdecl) = StringMap.find f func_decls in
 	    let actuals = List.rev (List.map (expr builder) (List.rev act)) in
@@ -135,11 +139,10 @@ in
 	| A.AssignDecl (*TODO*) 
 	| A.ArrAssign  (*TODO*) 
 	| A.AssignObj  (*TODO*)
-	| A.FunExp  (*TODO*) 
 	| A.KeyVal  (*TODO*) 
-	| A.ArrExp  (*TODO*) 
-	| A.Noexpr  (*TODO*)
-*)
+	| A.ArrExp  (*TODO*)
+*) 
+
     let add_terminal builder f =
       match L.block_terminator (L.insertion_block builder) with
 	Some _ -> ()
@@ -161,17 +164,19 @@ in
 	 let then_bb = L.append_block context "then" the_function in
 	 add_terminal (stmt (L.builder_at_end context then_bb) then_stmt)
 	   (L.build_br merge_bb);
+	 
 
  	 (*let elif_bb = L.append_block context "elif" the_function in
 	 add_terminal (stmt (L.builder_at_end context elif_bb) elif_stmt)
 	   (L.build_br merge_bb);*)
 
+	 (*ignore (L.build_cond_br bool_val then_bb elif_bb builder);*)
 	 let else_bb = L.append_block context "else" the_function in
 	 add_terminal (stmt (L.builder_at_end context else_bb) else_stmt)
 	   (L.build_br merge_bb);
+	 
+	 (*ignore (L.build_cond_br elif_bool_val elif_bb else_bb builder);*)
 
-	 (*ignore (L.build_cond_br bool_val then_bb elif_bb builder);
-	 ignore (L.build_cond_br elif_bool_val elif_bb else_bb builder);*)
 	 ignore (L.build_cond_br bool_val then_bb else_bb builder);
 	 L.builder_at_end context merge_bb
 
