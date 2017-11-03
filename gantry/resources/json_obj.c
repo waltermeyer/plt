8ade95517af892d4ae408b608eb6c2d299fdcd61
audreysc <audreyscopeland@gmail.com>
2017-11-03 07:37:23 -0400

    token objects don't update next prev pointers correctly, either because I'm doing it wrong, or because of the way alloca works. I do not have internet so will check on this later

--- gantry/resources/json_obj.c
+++ gantry/resources/json_obj.c
@@ -2,12 +2,36 @@
 #include <string.h>
 #include <alloca.h>
 
-struct JSON{
-  struct JSON *prev;
-  struct JSON *next;
-  struct JSON *child;
-  char *val;
+/*
+ * Sample JSON Object:
+ *
+ * { "Name": "Joe",
+ *   "Age" : 20,
+ *
+ *   "Classes" : ["PLT", "Class 2", "Class 3"]
+ *   "Contact Info": 
+ *  	{ "Address" : "22 Jumpstreet",
+ * 	  "Phone" : 9999999999 }
+ * }
+ *
+ */
 
+
+
+/* Keep track of where objects are located in memory.
+ * Stores pointers to the memory location of the first token in an object.
+ */
+struct json_list{
+  struct token *HEAD;
+};
+
+/* JSON token struct*/
+struct token{
+  struct token *prev;
+  struct token *next;
+  struct token *child;
+
+  char *val;
   int i;
   float f;
   int b;
@@ -15,25 +39,13 @@ struct JSON{
   char *key;
 };
 
-// { "Name": "Joe",
-//   "Age" : 20,
-//   "Classes" : ["PLT", "Class 2", "Class 3"]
-//   "Contact Info": 
-//  	{ "Address" : "22 Jumpstreet",
-// 	  "Phone" : 9999999999 }
-// }
-struct JSON * new_token () {
- 	struct JSON *token = alloca(sizeof(struct JSON));
-	token->key= NULL;
-	token->val=NULL;
-	token->prev = NULL;
-	token->next=NULL;
-	token->child=NULL;
-	return token;
+struct token * new_token () {
+ 	struct token *t = alloca(sizeof(struct token));
+	return t;
 }
 
 // This successfully updates the key of token 1 but not the sibling pointer
-int add_sibling(struct JSON *parent_token, struct JSON *sibling_token){
+int add_sibling(struct token *parent_token, struct token *sibling_token){
 	parent_token->next = sibling_token;
 	sibling_token->prev = parent_token;
 	parent_token->key = "Bob";
@@ -43,19 +55,36 @@ int add_sibling(struct JSON *parent_token, struct JSON *sibling_token){
 }
 
 
+// Based on the output I'm getting, I think there's a problem with how
+// I'm using alloca, because key values are being flushed fast, but
+// maybe there's really a problem in this code.
 int main(){
-	struct JSON *token1= new_token();
+	struct token *token1= new_token();
 	token1->key = "Joe";
 	printf("%s\n", token1->key);
-	
-	struct JSON *token2 = new_token();
+
+
+	struct token *token2 = new_token();
 	token2->key = "Age";
 	token2->i = 22;
 	
+	struct token *token3 = new_token();
+	token3->key = "Favorite Color";
+	token3->val = "Blue";
+	
+	token1->next = token3;
+	token3->prev = token1;
+
+	struct token *token1_from_token3 = token3->prev;
+	struct token *token3_from_token1 = token1->next;
+	printf("This is token 1 key from 3 in main: %s\n", token1_from_token3->key);
+	printf("This is token 1 key: %s\n", token1->key);
+	printf("This is token 3 key from 1 in main: %s\n", token3_from_token1->key);
+	printf("This is token 3 key: %s\n", token3->key);
 	//token1->next = token2;
 	add_sibling(token1, token2);
 	printf("%s\n", token1->key);
-	struct JSON *token1_again = token2->prev;
+	struct token *token1_again = token2->prev;
 	printf("%s\n", token1_again->key);
 	return 0;
 }
