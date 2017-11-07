eaa46ed1e95977c00475cf48bbc40ab6036bd9cf
audsc <audreyscopeland@gmail.com>
2017-11-07 11:00:37 -0500

    .out files instead of .gty.out

--- gantry/tests/generate_ref.sh
+++ gantry/tests/generate_ref.sh
@@ -2,14 +2,14 @@
 
 for file in ./test*.gty; do
 	if [[ `grep "****TEST****" $file` ]]; then
-	  sed -n -e '/\*\*\*\*TEST\*\*\*\*/,$p' $file | tail -n +2 | head -n -1 > $file.out
+	  sed -n -e '/\*\*\*\*TEST\*\*\*\*/,$p' $file | tail -n +2 | head -n -1 > ${file%.*}.out
 	  echo "Generated $(echo $file | cut -c 3-).out"
 	fi
 done
 
 for file in ./fail*.gty; do
 	if [[ `grep "****TEST****" $file` ]]; then
-	  sed -n -e '/\*\*\*\*TEST\*\*\*\*/,$p' $file | tail -n +2 | head -n -1 > $file.err
+	  sed -n -e '/\*\*\*\*TEST\*\*\*\*/,$p' $file | tail -n +2 | head -n -1 > ${file%.*}.err
 	  echo "Generated $(echo $file | cut -c 3-).err"
 	fi
 done
