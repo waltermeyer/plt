11c9a44e919ad636896cbb2ba55a7919cda32b83
audsc <audreyscopeland@gmail.com>
2017-11-07 10:25:33 -0500

    this doesn't work for generating files

--- gantry/tests/generate_ref.sh
+++ gantry/tests/generate_ref.sh
@@ -1,7 +1,9 @@
+#!/bin/bash  
+
 for file in ./test*.gty; do
  	[ -e "$file" ] || continue
 	touch ${file%.*}.out
-	grep -v '^\s*$' "$file"|sed -r 's/^.{10}//' | tail -n1 > ${file%.*}.out
+	sed -n -e '/####TEST####/,$p' $file | tail -n +2 > ${file%.*}.out
 done
 
 for file in ./fail*.gty; do
