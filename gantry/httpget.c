75891265a7bc9939cade632661fe11d4b9e5b591
TSamee <taimur.samee@gmail.com>
2017-11-08 17:55:03 -0500

    Packaged httpget into its own function; still need to write get request output to a string

--- gantry/httpget.c
+++ gantry/httpget.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <curl/curl.h>
 
-int main(void) {
+void httpget(char *url) {
 
 	CURL *curl;
-	CURLcode res;	// Error codes
+	CURLcode res;		// Error codes
+	char outbuf[4096];	// Holds file contents
 
 	curl_global_init(CURL_GLOBAL_DEFAULT);
 	curl = curl_easy_init();
-	curl_easy_setopt(curl, CURLOPT_URL, "http://google.com");
+	curl_easy_setopt(curl, CURLOPT_URL, url);
 
 	res = curl_easy_perform(curl);
 	if (res != CURLE_OK) {
-		fprintf(stderr, "curl_easy_perform() failed: %s\n",
+		fprintf(stderr, "httpget: curl_easy_perform() failed: %s\n",
 			curl_easy_strerror(res));
 	}
-
+	
 	curl_easy_cleanup(curl);
+}	
 
+
+int main() {
+
+
+	httpget("https://curl.haxx.se/lubcurl/c/");
 	return 0;
 }
+
+
+
