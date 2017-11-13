921412239c6d7d3f9419d1fab1f8021542cdccfa
audsc <audreyscopeland@gmail.com>
2017-11-13 17:56:42 -0500

    String slice function works on its own

--- gantry/string_slice.c
+++ gantry/string_slice.c
@@ -8,24 +8,26 @@ char *string_slice(int begin, int end, char *src){
 	char *dest = malloc(len + 1);
 	int i;
 	int dest_i = 0;
-	for (i=begin;i<end && src[i]!='\0'; i++)
+	for (i=begin;i<end&&src[i]!='\0' ; i++){
 		dest[dest_i] = src[i];
 		dest_i++;
-	for (;i<end;i++)
+	}
+	for (; i< end; i++){
 		dest[dest_i] = '\0';
-		dest_i++;
+	}
 	return dest;
 }
 
 
+#ifdef BUILD_TEST
 int main(){
 	char *src = "this is a string";
 	char *dest = string_slice(1, 3, src);
 	printf("%s\n", dest);
-	src = "foobar";
 	dest = string_slice(0, 10, src);
 	printf("%s\n", dest);
-	//dest = string_slice(4, 20, src);
-	//printf("%s\n", dest);
+	dest = string_slice(4, 20, src);
+	printf("%s\n", dest);
 	return 0;
 }
+#endif
