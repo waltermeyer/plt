3233fc0e8043a362de6713292207e686bc13b056
audsc <audreyscopeland@gmail.com>
2017-11-13 19:39:47 -0500

    switch order of slice args to match proposal

--- gantry/slice.c
+++ gantry/slice.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 
 
-char *slice(int begin, int end, char *src){
+char *slice(char *src, int begin, int end){
 	int len = end - begin;
 	char *dest = malloc(len + 1);
 	int i;
@@ -23,11 +23,11 @@ char *slice(int begin, int end, char *src){
 #ifdef BUILD_TEST
 int main(){
 	char *src = "this is a string";
-	char *dest = slice(1, 3, src);
+	char *dest = slice(src, 1, 3);
 	printf("%s\n", dest);
-	dest = slice(0, 10, src);
+	dest = slice(src, 0, 10);
 	printf("%s\n", dest);
-	dest = slice(4, 20, src);
+	dest = slice(src, 4, 20);
 	printf("%s\n", dest);
 	return 0;
 }
