ec4459728efd73c49411f175820ff9da04bd355b
TSamee <taimur.samee@gmail.com>
2017-12-08 20:08:09 -0500

    Trying to change Binop to case i1 up to i8 for Booleans

--- gantry/codegen.ml
+++ gantry/codegen.ml
@@ -281,8 +281,9 @@ let translate (globals, functions) =
         let e1' = expr builder e1
         and e2' = expr builder e2 in
 	let typ = L.string_of_lltype (L.type_of e1') in
+        print_endline typ;
 	(match typ with 
-           "i32" | "i8"  -> (match op with
+           "i32" -> (match op with
              A.Add  -> L.build_add
            | A.Sub  -> L.build_sub
            | A.Mult -> L.build_mul
@@ -310,6 +311,14 @@ let translate (globals, functions) =
            | A.Geq  -> L.build_fcmp L.Fcmp.Oge
 	   | _      -> raise (Failure ("Invalid float binary operation"))
            ) e1' e2' "tmp" builder
+        |  "i8" | "i1"  -> (match op with
+             A.And  -> L.build_intcast (L.build_and e1' e2' "tmp" builder)
+           | A.Or   -> L.build_intcast (L.build_or e1' e2' "tmp" builder)
+           | A.Eq   -> L.build_intcast (L.build_icmp L.Icmp.Eq e1' e2' "tmp" builder)
+           | A.Neq  -> L.build_intcast (L.build_icmp L.Icmp.Ne e1' e2' "tmp" builder)
+           | _      -> raise (Failure ("Invalid boolean binary operation"))
+           ) i8_t "tmp" builder 
+
         | "i8*" -> (match op with
             A.Eq    ->  L.build_icmp L.Icmp.Eq
                        (L.build_call stringeq [| (e1') ; (e2') |] "stringeq" builder)
